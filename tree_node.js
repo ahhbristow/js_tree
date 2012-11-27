c4a2f03979d0387a71bf71e3625fc301f5ce24a4
ahhbristow <ahhbristow@hotmail.com>
2012-11-27 00:07:26 +0000

    Children teasers are now working, and they can be fixed to the tree permanently

--- tree_node.js
+++ tree_node.js
@@ -15,40 +15,63 @@ Tree.prototype.init = function() {
 	   height: document.height
 	});
 
-	this.layer = new Kinetic.Layer();
+	this.layer      = new Kinetic.Layer();
+	this.line_layer = new Kinetic.Layer();
 
 	// Create the tree
-	var node = new Node(50,50,null,this);
+	var node = new Node(300,200,null,this,"DISPLAYED");
 	for(i = 0; i < 3; i++) {
-		var child_node = new Node((i*50),100,node,this);
+		var child_node = new Node((i*100)+300,300,node,this,"HIDDEN");
 		node.addChild(child_node);
 		for(j = 0; j < 3; j++) {
-			var grandchild_node = new Node(((i*50) + (j*50)),200,child_node,this);
+			var grandchild_node = new Node(((i*100) + (j*100))+300,400,child_node,this,"HIDDEN");
 			child_node.addChild(grandchild_node);
 		}
 	}
         // add the layer to the stage
+        stage.add(this.line_layer);
         stage.add(this.layer);
 }
 
 Tree.prototype.addKineticObject = function(kinetic_object) {
 	this.layer.add(kinetic_object);
+	kinetic_object.moveToTop();
 }
+Tree.prototype.addLineObject = function(line_object) {
+	this.layer.add(line_object);
+}
+
+Tree.prototype.redraw = function() {this.layer.draw()}
 
 /***************************************
  *
  */
-function Node(x,y,parent_node,tree) {
-	this.init(x,y,parent_node,tree);
-}
+function Node(x,y,parent_node,tree,display_status) {
 
-
-Node.prototype.init = function(x,y,parent_node,tree) {
+	/**************************
+	 * Declare private members
+	 */
+	var x,y;
+	var tree;
+	var parent_node;
+	var children;
 	
-	// Init the fields
+	var kinetic_obj, line_obj;
+
+
+	// Status members
+	var children_showing;
+	var children_fixed;	
+	var display_status;
+
+	/**************************
+	 * Initialise this instance
+	 */
 	this.x = x;
 	this.y = y;
 	this.parent_node = parent_node;
+	this.display_status = display_status;
+	this.children = new Array();
 
 	// Each node needs to store a reference
 	// to the tree it's part of
@@ -61,11 +84,11 @@ Node.prototype.init = function(x,y,parent_node,tree) {
 		// to the parent node
 		this.line_obj = new Kinetic.Line({
 		   points: [parent_node.getX(), parent_node.getY(), x, y],
-		   strokeWidth: 2,
+		   strokeWidth: 4,
 		   lineCap: "round",
 		   lineJoin: "round"
 		});
-		this.tree.addKineticObject(this.line_obj);
+		this.tree.addLineObject(this.line_obj);
 	}
 
 
@@ -76,23 +99,40 @@ Node.prototype.init = function(x,y,parent_node,tree) {
           y: this.y,
           radius: 30,
           fill: 'red',
-          stroke: 'black',
+          stroke: '#ccc',
 	  strokeWidth: 4,
+          visible: false,
 	  draggable: true
 	});
-	//kinetic_layer.add(this.kinetic_obj);
+
+	// Add this node's kinetic JS object to the
+	// containing tree
 	this.tree.addKineticObject(this.kinetic_obj);
 
 	this.kinetic_obj.on("mousedown",function() {
-		//node.select(null);
 	});
 	this.kinetic_obj.on("mouseup",function() {
+	});
+	this.kinetic_obj.on("click",function() {
+		node.click();
+		node.tree.redraw();
+	});
+	this.kinetic_obj.on("mouseenter",function() {
+		node.hover();
+		node.tree.redraw();
+	});
+	this.kinetic_obj.on("mouseleave",function() {
+		node.unHover();
+		node.tree.redraw();
 	});
 	this.kinetic_obj.on("dragmove",function() {
 		node.move();
 	});
 
-	this.children = new Array();
+	// Set the display status of this node
+	this.setDisplayStatus(display_status);
+
+
 }
 
 
@@ -163,19 +203,139 @@ Node.prototype.addChild = function(child) {
 	this.children.push(child);
 }
 
+/*
+ * A Node can have one of three display statuses:
+ *
+ * DISPLAYED: This node is permanently displayed
+ * TEASER:    This node is temporarily displayed as a teaser
+ *            of its parent's children.
+ * HIDDEN:    This node isn't displayed
+ */
 Node.prototype.setDisplayStatus = function(status) {
 
 	if (status == "DISPLAYED") {
-		// This node should be displayed
-		this.kinetic_obj.setFill("blue");
-		this.line_obj.setFill("blue");
+
+		this.display_status = "DISPLAYED";
+		
+		// This node may be currently hidden,
+		// so show it.	
+		this.kinetic_obj.show();
+		this.kinetic_obj.setFill("#444");
+		this.kinetic_obj.setStroke("#333");
+
+		if (this.line_obj) {
+			this.line_obj.show();
+			this.line_obj.setFill("#000044");
+			this.line_obj.setStroke("#333");
+		}
+
 	} else if (status == "TEASER") {
-		// Show this node as a teaser
+	
+		// Used for other logic
+		this.display_status = "TEASER";
+	
+		// This node may be currently hidden,
+		// so show it.	
+		this.kinetic_obj.show();
+		this.kinetic_obj.setFill("blue");
+		
+		if(this.line_obj) {
+			this.line_obj.show();
+			this.line_obj.setFill("blue");
+		}
+
+
 	} else if (status == "HIDDEN") {
 		// Hide this node and it's subtree
+		this.kinetic_obj.hide();
+		
+		if(this.line_obj) {
+			this.line_obj.hide();
+		}
 	} else {
 		// Do nothing
 	}
+}
+
+/*
+ * Display all the children as a teaser,
+ * not permanently displaying them
+ */
+Node.prototype.showChildren = function() {
+	// Loop throw all the children
+	// and set their displayed status
+	// to TEASER
+	//
+	var child;
+	for (var i = 0; i < this.children.length; i++) {
+		child = this.children[i];
+		child.setDisplayStatus("TEASER");
+	}
+	this.children_showing = 1;	
+}
+Node.prototype.hideChildren = function() {
+	// Loop throw all the children
+	// and set their displayed status
+	// to TEASER
+	var child;
+	for (var i = 0; i < this.children.length; i++) {
+		child = this.children[i];
+		child.setDisplayStatus("HIDDEN");
+	}
+	this.children_showing = 0;	
+}
+
+
+/*
+ * Permanently attaches the children to
+ * the tree
+ */
+Node.prototype.fixChildren = function() {
+	var child;
+	for (var i = 0; i < this.children.length; i++) {
+		child = this.children[i];
+		child.setDisplayStatus("DISPLAYED");
+	}
+	this.children_fixed = 1;
+	this.children_showing = 0;
+}
+
+
+
+/****************************************
+ * Event handler functions
+ */
+
+Node.prototype.hover = function() {
+	
+	// Modify this node
+	this.kinetic_obj.moveToTop();
+	this.kinetic_obj.setFill('#cc0000');
+
+	// Do something with the children
+	if(!this.children_fixed) {
+		this.showChildren();
+	}
+}
+
+Node.prototype.unHover = function() {
+	
+	// Modify this node
+	this.kinetic_obj.setFill('444');
+
+	// Do something with the children
+	if (!this.children_fixed) {
+		this.hideChildren();
+	}
+}
+
+Node.prototype.click = function() {
+	
+	// If the children are showing and aren't
+	// already attached,fix them to the tree permanently
+	if (this.children_showing && !this.children_fixed) {
+		this.fixChildren();	
+	}	
 }
 
 /****************************************
